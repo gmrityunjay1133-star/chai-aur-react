d200b90c3f2ccd3e2da37bde96abbad892c19f7f
Mrityunjay Gupta <gmrityunjay1133@gmail.com>
2026-07-26 23:24:35 +0530

    changes

--- 09themeSwitcher/vite.config.js
+++ 09themeSwitcher/vite.config.js
@@ -3,7 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
+  
+  
+  plugins: [
   tailwindcss(),
 ],
+  darkMode: 'class',
 })
